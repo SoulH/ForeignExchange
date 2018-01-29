d82ba6fd1d1c1cf01f1b5d20e7b60b9fe92ce371
SAUL HERNANDEZ <saul7.hernandez@outlook.com>
2018-01-29 16:19:01 -0400

    ExchageRateEntityModel

---
+++ ForeignExchange/ForeignExchange/ForeignExchange.Android/Implementations/SQLiteConfig.cs
@@ -0,0 +1,15 @@
+﻿using App.DependencyServices;
+using App.Droid.Implementations;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteConfig))]
+namespace App.Droid.Implementations
+{
+    public class SQLiteConfig : ISQLiteConfig
+    {
+        public string DbDirectory()
+        {
+            return System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+        }
+    }
+}
---
+++ ForeignExchange/ForeignExchange/ForeignExchange/Data/Entities/ExchangeRate.cs
@@ -0,0 +1,19 @@
+﻿using ForeignExchange.Data.Prototypes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForeignExchange.Data.Entities
+{
+    public class ExchangeRate : Entity
+    {
+        [Unique(Name ="Rate", Order= 1)]
+        public int FromCoinId { get; set; }
+
+        [Unique(Name = "Rate", Order = 2)]
+        public int ToCoinId { get; set; }
+
+        public decimal Rate { get; set; }
+    }
+}
